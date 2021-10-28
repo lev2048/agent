58261880d26cafed1ad3e5b9720ce8348f1cd0eb
lev <lev@shield.asia>
2021-10-28 15:38:30 +0000

    fix

--- agent.go
+++ agent.go
@@ -360,12 +360,12 @@ func (at *Agent) numberCover(data, nType string) (value interface{}) {
 func UnitConver(sizeInByte float64) string {
 	suffixes := [5]string{"B", "KB", "MB", "GB", "TB"}
 	base := math.Log(sizeInByte) / math.Log(1024)
-	getSize := Round(math.Pow(1024, base-math.Floor(base)), .5, 2)
+	getSize := round(math.Pow(1024, base-math.Floor(base)), .5, 2)
 	getSuffix := suffixes[int(math.Floor(base))]
 	return strconv.FormatFloat(getSize, 'f', -1, 64) + " " + string(getSuffix)
 }
 
-func Round(val float64, roundOn float64, places int) (newVal float64) {
+func round(val float64, roundOn float64, places int) (newVal float64) {
 	var round float64
 	pow := math.Pow(10, float64(places))
 	digit := pow * val
